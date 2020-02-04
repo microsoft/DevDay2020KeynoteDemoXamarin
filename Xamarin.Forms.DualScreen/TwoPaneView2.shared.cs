5a2370a5dc4c40efb3aae23d5d85c467f22f5855
Shane Neuville <shneuvil@microsoft.com>
2020-02-03 18:42:41 -0700

    - remove

--- Xamarin.Forms.DualScreen/TwoPaneView2.shared.cs
+++ Xamarin.Forms.DualScreen/TwoPaneView2.shared.cs
@@ -5,18 +5,6 @@ namespace Xamarin.Forms.DualScreen
 {
     public partial class TwoPaneView
 	{
-
-		static TwoPaneView()
-		{
-#if UWP
-			// Without this the service gets linked out when compiling with the native tool chain on UWP
-			DependencyService.Register<DualScreenService>();
-#elif !ANDROID
-			DependencyService.Register<NoDualScreenServiceImpl>();
-#endif
-		}
-
-
         //TODO replace with OnIsPlatformEnabledChanged once we have NUGET and IVT is turned on
         bool _isconnected = false;
         protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
